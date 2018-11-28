33b5b09fcbdfafbdec802cac5af6fb338c938d1d
slashdims <45331168+slashdims@users.noreply.github.com>
2018-11-29 03:53:10 +0900

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -213,36 +213,57 @@ $(window).load(function() {
 		});
 	});
 });
-//Form
-let statusMessage = document.createElement('div');
+//Popup
+let popup = document.querySelector('.overlay')
+
+let popupClose = document.querySelector('.popup-close')
+console.log(popup)
+
+document.body.addEventListener('submit', (e) => {  
+    popup.style.display = 'block';
+    e.preventDefault();
+    for(let i = 0; i < input.length; i++){
+                input[i].value = '';
+    }
+});
+
+
+popup.addEventListener('click', (e)=>{
+    popup.style.display = 'none'; 
+});
+popupClose.addEventListener('click', (e)=>{
+    popup.style.display = 'none'; 
+});
+//form
+
 
 input = document.querySelectorAll('input');
 
 document.body.addEventListener('submit', (e) => { 
-    let target = e.target;
-    e.preventDefault();
+	let target = e.target;
+	e.preventDefault();
 
    
 });
 $(document).ready(function() {
-    $('#form').submit(function(e) { 
-        e.preventDefault()
-        $.ajax({
-            type: "POST",
-            url: "mail.php",
-            data: $(this).serialize()
-        }).done(function() {
-            console.log('Ок');
-            $(this).find('input').val('');
-            $('#form').trigger('reset');
-        });
-        return false;
-    });
+	$('#form').submit(function(e) { 
+		e.preventDefault(); 
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			console.log('Ок');
+			$(this).find('input').val('');
+			$('#form').trigger('reset');
+		});
+		return false;
+	});
 });
 
-document.body.addEventListener('submit', (e) => { 
+document.body.addEventListener('submit', (e) => {  
     e.preventDefault();
-    for(let i = 0; i < input.length; i++){
-        input[i].value = '';
-    }
+	for(let i = 0; i < input.length; i++){
+				input[i].value = '';
+	}
 });
